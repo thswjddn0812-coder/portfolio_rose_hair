7bc4652af3fd8565d8e198bd36533a07e14f622d
sonjeongwoo <thswjddn0812@gmail.com>
2026-01-16 16:17:56 +0900

    2026-01-16두번째 커밋

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // 이 줄을 반드시 추가하세요!
+  images: {
+    unoptimized: true, // Cloudflare Pages 정적 배포 시 이미지 최적화 경고 방지
+  },
 };
 
 export default nextConfig;
